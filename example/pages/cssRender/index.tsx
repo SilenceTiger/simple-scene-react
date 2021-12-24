3f4e2e374e0eaf88fd20091de453541bae75cce3
silencetiger <547989722@qq.com>
2021-12-24 16:01:35 +0800

    css render补充

--- example/pages/cssRender/index.tsx
+++ example/pages/cssRender/index.tsx
@@ -11,6 +11,7 @@ import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass';
 import { ScanShader3 as ScanShader } from './shader';
+import anime from 'animejs/lib/anime.es.js';
 
 let cssRender = new CSS3DRenderer();
 let cube: THREE.Mesh;
@@ -59,6 +60,107 @@ const addMesh = async (scene: THREE.Scene) => {
   scene.add(cube);
 };
 
+const elements:any = [
+  require(`./imgs/1.png`),
+  require(`./imgs/2.png`),
+  require(`./imgs/3.png`),
+  require(`./imgs/4.png`),
+  require(`./imgs/5.png`),
+  require(`./imgs/6.png`),
+  require(`./imgs/7.png`),
+  require(`./imgs/8.png`),
+  require(`./imgs/9.png`),
+  require(`./imgs/10.png`),
+  require(`./imgs/11.png`),
+  require(`./imgs/12.png`),
+  require(`./imgs/13.png`),
+  require(`./imgs/14.png`),
+  require(`./imgs/15.png`),
+  require(`./imgs/16.png`),
+  require(`./imgs/17.png`),
+  require(`./imgs/18.png`),
+  require(`./imgs/19.png`),
+  require(`./imgs/20.png`),
+  require(`./imgs/21.png`),
+  require(`./imgs/22.png`),
+  require(`./imgs/23.png`),
+  require(`./imgs/24.png`),
+];
+
+
+let r = 500;
+let status = 'plane';
+const addElement = (CssContainerObject: any) => {
+  const btn = document.createElement('button');
+  btn.innerHTML = 'animate';
+  btn.style.position = 'absolute';
+  btn.style.top = '20px';
+  btn.style.left = '20px';
+  document.body.appendChild(btn);
+  btn.addEventListener('click', () => {
+    CssContainerObject.children.forEach((obj, index) => {
+      let row = Math.floor(index / 8);
+      let col = index % 8;
+      let cita = (col * Math.PI) / 4;
+      if (status === 'plane') {
+        anime({
+          targets: obj.position,
+          x: r * Math.cos(cita),
+          z: r * Math.sin(cita),
+          duration: 4000,
+          delay: Math.random() * 2000,
+          easing: 'spring',
+          // loop: true,
+          // autoplay: false,
+        });
+        anime({
+          targets: obj.rotation,
+          y: Math.PI / 2 - cita,
+          duration: 4000,
+          delay: Math.random() * 2000,
+        });
+ 
+      }
+      else {
+        anime({
+          targets: obj.position,
+          x: -800 + col * 230,
+          y: 300 - row * 230,
+          z: 0,
+          duration: 4000,
+          delay: Math.random() * 2000,
+          easing: 'spring',
+        });
+        anime({
+          targets: obj.rotation,
+          y: 0,
+          duration: 4000,
+          delay: Math.random() * 2000,
+        });
+      }
+    });
+    status = status === 'plane' ? 'circle' : 'plane';
+  });
+
+  for (let i = 0; i < elements.length; i++) {
+    const card = document.createElement('div');
+    card.style.width = '200px';
+    card.style.height = '200px';
+    card.style.color = '#0000ff';
+
+    const img = document.createElement('img');
+    img.style.height = '100%';
+    img.style.width = '100%';
+    img.src = elements[i];
+    card.appendChild(img);
+    const cardObject = new CSS3DObject(card);
+    let row = Math.floor(i / 8);
+    let col = i % 8;
+    cardObject.position.set(-800 + col * 230, 300 - row * 230, 0); // plane
+    CssContainerObject.add(cardObject);
+  }
+};
+
 const initCssRender = (
   container: any,
   width: number,
@@ -73,15 +175,7 @@ const initCssRender = (
   const CssContainer = document.createElement('div');
   const CssContainerObject = new CSS3DObject(CssContainer);
   scene.add(CssContainerObject);
-  const card = document.createElement('div');
-  card.style.width = '200px';
-  card.style.height = '50px';
-  card.style.backgroundColor = '#fff';
-  card.style.color = '#0000ff';
-  card.innerText = 'hello world';
-  const cardObject = new CSS3DObject(card);
-  cardObject.position.set(-500, 0, 0);
-  CssContainerObject.add(cardObject);
+  addElement(CssContainerObject);
 };
 
 const Scene3D = () => {
@@ -93,10 +187,10 @@ const Scene3D = () => {
     height: number
   ) => {
     // init css render
-    console.log(target.renderer.domElement);
-    target.renderer.domElement.style.position = "absolute";
-    target.renderer.domElement.style.top = "0px";
-    target.renderer.domElement.style.left = "0px";
+    camera.position.set(0, 200, 500);
+    target.renderer.domElement.style.position = 'absolute';
+    target.renderer.domElement.style.top = '0px';
+    target.renderer.domElement.style.left = '0px';
     initCssRender(target.container, width, height, scene);
     addLight(scene);
     await addMesh(scene);
@@ -161,7 +255,7 @@ const Scene3D = () => {
       style={{
         background: '#000',
       }}
-      showAxisHelper={true}
+      showAxisHelper={false}
       beforeRender={beforeRender}
       animate={animate}
       addPass={addPass}
